5c8b8e8f4a74c68fd5c36732a33c2d594fcfc208
Baga6312 <magroubertn@gmail.com>
2024-06-03 16:42:09 +0100

    changes

--- scraping-script/testing.py
+++ scraping-script/testing.py
@@ -3,10 +3,15 @@ from bs4 import BeautifulSoup
 
 request = RequestsTor(tor_ports=(9050,) , tor_cport=9051)
 
-url ="http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion"
+url =""
 
 keyword ="actia"
 
+          #######################
+          ##                   ##
+          ##        AHMIA      ##
+          ##                   ##
+          #######################
 
 def SCRAP_AHMIA (url , keyword) : 
     online_link = []
@@ -17,30 +22,102 @@ def SCRAP_AHMIA (url , keyword) :
         online_link.append(site.get_text())
     
     for link in online_link : 
-        print (link)
-        rq1 = request.get("http://" +link)
 
-        if str(rq1) == "<Response [200]>" : 
+        try : 
+
+            rq1 = request.get("http://" +link)
+            rq1.raise_for_status()
             soup = BeautifulSoup(rq1.text, 'html.parser')
             keyword_occ = soup.find(keyword)
     
             if keyword_occ  : 
-                print (keyword + " found in " + link)
+                print ("found")
+                break 
             else : 
-                print ("not found ")
-    
-        elif str(rq1) == "<Response [200]>" : 
-            print ("site is unreacheable")
+                print ("not found")
 
+        except Exception :
+            print("site is unreachable")
 
-
+         #######################
+         ##                   ##
+         ##    Relate_List    ##
+         ##                   ##
+         #######################    
+       
 def SCRAP_RELATE_LIST (url , keyword) : 
-    rq = request.get(url+'/'+keyword+'/') 
-    soup = BeautifulSoup(rq.text , 'html.parser').find_all("span" , class_="title")[0].get_text()
 
-    if soup == "Nothing found for '"+keyword+"'" : 
-        return "nothing to be found"
-    else : 
-        return "found in relateList"
+    try : 
+        rq = request.get(url+'/'+keyword+'/') 
+        soup = BeautifulSoup(rq.text , 'html.parser').find_all("span" , class_="title")[0].get_text()
+    
+        if str(soup) == "Nothing found for '"+keyword+"'" : 
+            print ("not found")
+        else : 
+            print ("found")
+    
+    except Exception :
+        print("site is unreachable")
 
-SCRAP_AHMIA(url, keyword )
+
+         #######################
+         ##                   ##
+         ##    Ransom_EXX     ##
+         ##                   ##
+         #######################    
+
+def SCRAP_RANSOM_EXX (url , keyword , pages , n ) : 
+
+        try:
+            urll ="http://" + url + pages + "/index.html"
+            print (urll)
+            rq = request.get(urll )
+            soup = BeautifulSoup(rq.text, 'html.parser').find_all("a", href=True)
+            print(rq)
+
+            found = False
+            for sip in soup:
+                if sip.get_text() == keyword:
+                    print("found")
+                    found = True
+                    break
+
+            if not found:
+                print("not found")
+
+            n += 1
+            pages = "/page/" + str(n)
+            SCRAP_RANSOM_EXX(url, keyword, pages, n)
+
+        except Exception as e:
+            print("site is unreachable" , e)
+    
+
+         #######################
+         ##                   ##
+         #  Dark_Leak_Market  ##
+         ##                   ##
+         #######################
+
+def SCRAP_DARK_LEAK_MARKET (url , keyword ) : 
+
+    found_not_found = "not found"
+    try : 
+        rq = request.get("http://"+url)
+        soup = BeautifulSoup(rq.text , 'html.parser').find_all("a" , href=True)   
+    
+        for sip in soup : 
+            souper = sip.get_text()
+            if keyword not in souper.split(" ") :
+               pass 
+            
+            else  : 
+                found_not_found = "found"
+                break 
+
+            
+    except Exception : 
+        print('site is unreachable')
+    print(found_not_found)
+
+SCRAP_DARK_LEAK_MARKET(url , keyword)
