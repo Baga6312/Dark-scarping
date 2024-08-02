01597dbf520cf956dd17fd5da7bb168b78c48d2c
Baga6312 <magroubertn@gmail.com>
2024-08-02 13:22:40 +0100

    configure

--- configure.sh
+++ configure.sh
@@ -1,133 +1,90 @@
-#!/bin/sh
+#!/bin/bash
 
 install_package() {
-    distro=$(cat /etc/*release* | grep "^ID=" | cut -d '=' -f 2 | tr -d '"')
-    package_name="tor python3 python-pip" 
+    distro=$(awk -F= '/^ID=/{print $2}' /etc/*release* | tr -d '"')
+    package_name="tor python3 python3-pip"
     case "$distro" in
-        "ubuntu" | "debian")
+        "ubuntu"|"debian")
             echo "Installing on $distro..."
             sudo apt-get update
             sudo apt-get install -y $package_name
-            echo "packages has been installed successfully"
             ;;
-        "centos" | "rhel" | "fedora")
+        "centos"|"rhel"|"fedora")
             echo "Installing on $distro..."
             sudo yum install -y $package_name
-            echo "packages has been installed successfully"
             ;;
         "arch")
             echo "Installing on $distro..."
-            sudo pacman -S --noconfirm  $package_name 
-            echo ""
-            echo ""
-            echo "packages has been installed successfully"
-            echo ""
-            echo ""
+            sudo pacman -S --noconfirm $package_name
             ;;
         *)
             echo "Unsupported distribution: $distro"
+            exit 1
             ;;
     esac
+    echo "Packages have been installed successfully"
 }
 
-install_dependecies (){ 
-    
-    distro=$(cat /etc/*release* | grep "^ID=" | cut -d '=' -f 2 | tr -d '"')
-    case "$distro" in
-        "arch")
-            echo "Installing dependecies ..."
-            pip3 install -r requirement.txt --break-system-packages
-            echo ""
-            echo ""
-            echo "finish installing dependecies"
-            echo ""
-            echo ""
-            ;;
-        *)
-            pip3 install -r requirement.txt 
-            ;;
-    esac
+install_dependencies() {
+    distro=$(awk -F= '/^ID=/{print $2}' /etc/*release* | tr -d '"')
+    echo "Installing dependencies..."
+    if [ "$distro" = "arch" ]; then
+        pip3 install -r requirements.txt --break-system-packages
+    else
+        pip3 install -r requirements.txt
+    fi
+    echo "Finished installing dependencies"
 }
 
 enable_daemon() {
-    
-    sleep 2 
-    echo "enabling tor ..."
-    echo ""
-    echo ""
-
-
+    echo "Enabling Tor..."
     if [ -f /etc/os-release ]; then
         . /etc/os-release
         OS=$NAME
     elif [ -f /etc/redhat-release ]; then
-        OS=$(cat /etc/redhat-release | awk '{print $1}')
+        OS=$(awk '{print $1}' /etc/redhat-release)
     else
         echo "Unsupported OS"
-    fi
-
-    if [ "$OS" = "Ubuntu" ] || [ "$OS" = "Debian GNU/Linux" ] ; then
-        sudo systemctl enable tor
-        echo "tor is enabled on boot "
-    elif [ "$OS" = "Arch Linux" ] ; then
-        sudo systemctl enable tor
-        echo "tor is enabled on boot "
-    elif [ "$OS" = "CentOS" ] || [ "$OS" = "Red Hat Enterprise Linux Server" ] ; then
-        sudo service tor start
-        echo "tor is enabled on boot "
-    else
-        echo "Unsupported OS: $OS"
         exit 1
     fi
-    sleep 2 
+
+    case "$OS" in
+        "Ubuntu"|"Debian GNU/Linux"|"Arch Linux")
+            sudo systemctl enable tor
+            ;;
+        "CentOS"|"Red Hat Enterprise Linux Server")
+            sudo chkconfig tor on
+            ;;
+        *)
+            echo "Unsupported OS: $OS"
+            exit 1
+            ;;
+    esac
+    echo "Tor is enabled on boot"
 }
 
 update_torrc() {
-
-    
-    echo ""
-    echo ""
-    echo "configuring /etc/tor/torrc file "
-    echo ""
-    echo ""
-
+    echo "Configuring /etc/tor/torrc file"
     if [ -f "/etc/tor/torrc" ]; then
-        sudo sed -i '/^#ControlPort/s/#//' /etc/tor/torrc
-        sudo sed -i '/^ControlPort/s/9051/1/' /etc/tor/torrc
-        
-        sudo sed -i '/^#CookieAuthentication/s/#//' /etc/tor/torrc
-        sudo sed -i '/^CookieAuthentication/s/0/1/' /etc/tor/torrc
-        sleep 2 
-        echo "done configuring tor file "
-        echo ""
-        echo ""
-    
+        sudo sed -i 's/^#ControlPort 9051/ControlPort 9051/' /etc/tor/torrc
+        sudo sed -i 's/^#CookieAuthentication 0/CookieAuthentication 1/' /etc/tor/torrc
+        echo "Done configuring Tor file"
     else
         echo "Tor configuration file not found."
         exit 1
     fi
 }
 
-
 restart_tor_with_hashed_password() {
     read -s -p "Enter password for Tor ControlPort: " password
     echo
-
     hashed_password=$(tor --hash-password "$password")
-
     if [ -f "/etc/tor/torrc" ]; then
-
-        sudo sed -i '/^#ControlPort/s/#//' /etc/tor/torrc
-        sudo sed -i '/^ControlPort/s/9051/1/' /etc/tor/torrc
-
+        sudo sed -i 's/^#ControlPort 9051/ControlPort 9051/' /etc/tor/torrc
         sudo sed -i "s/^HashedControlPassword.*$/HashedControlPassword $hashed_password/" /etc/tor/torrc
-
         sudo systemctl restart tor
-        sleep 1 
-        echo "end configuring torrc file" 
-        echo "" 
-        echo ""
-        echo "for executing the python script "
+        echo "Finished configuring torrc file"
+        echo "The Python script can now be executed"
     else
         echo "Tor configuration file not found."
         exit 1
@@ -136,10 +93,10 @@ restart_tor_with_hashed_password() {
 
 main() {
     install_package
-    install_dependecies 
+    install_dependencies
     enable_daemon
     update_torrc
     restart_tor_with_hashed_password
 }
 
-main 
+main
